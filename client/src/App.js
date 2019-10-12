1611d7307d538a4fbb927486430d61a9e850ffda
Oladipo Osinubi <diposinubi@gmail.com>
2019-10-12 11:46:52 -0400

    added logos

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,13 @@ import MerchandiseList from './components/MechandiseList.js'
 import ScheduleList from './components/ScheduleList.js'
 import './App.css';
 
+const styling ={
+  logo: {
+    width: "150px",
+    height: "100px"
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -26,7 +33,22 @@ function App() {
       <footer className="footer">
         <div className="footerContainer">
           <div>
-            <a href="https://www.premierleague.com/tables" target="_blank">EPL</a>
+            <a href="https://www.premierleague.com/tables" target="_blank"><img src="./epl.png" style={styling.logo}/></a>
+          </div>
+          <div>
+            <a href="https://www.skysports.com/la-liga-table" target="_blank"><img src="./laliga.png" style={styling.logo}/></a>
+          </div>
+          <div>
+            <a href="https://www.mlssoccer.com/standings" target="_blank"><img src="./mls.png" style={styling.logo}/></a>
+          </div>
+          <div>
+            <a href="https://www.bbc.com/sport/football/french-ligue-one/table" target="_blank"><img src="./league1.png" style={styling.logo}/></a>
+          </div>
+          <div>
+            <a href="https://www.skysports.com/serie-a-table" target="_blank"><img src="./SerieA.png" style={styling.logo}/></a>
+          </div>
+          <div>
+            <a href="https://www.skysports.com/eredivisie-table" target="_blank"><img src="./eredivisie.png" style={styling.logo}/></a>
           </div>
         </div>
       </footer>
